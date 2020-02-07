6c7ddfffe1f9b303d6d4d7c298a04a034d33be2c
lbj <1147431798@qq.com>
2020-02-08 02:57:10 +0800

    7st chapter-表单：添加，删除，编辑电影条目

--- helloFlask/app.py
+++ helloFlask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import request,url_for,redirect,flash
+from flask import render_template, Flask
 from dotenv import find_dotenv, load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 import os,sys
@@ -21,6 +22,9 @@ app = Flask(__name__)
 #配置变量的名称必须使用大写，写入配置的语句一般会放到扩展类实例化语句之前
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False          # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'   #flash() 函数在内部会把消息存储到 Flask 提供的 session 对象里。session 用来在请求间存储数据，
+                                   #它会把数据签名后存储到浏览器的 Cookie 中，所以我们需要设置签名所需的密钥
+                                   #这个密钥的值在开发时可以随便设置。基于安全的考虑，在部署时应该设置为随机字符，且不应该明文写在代码里， 在部署章节会详细介绍
 
 db = SQLAlchemy(app)  #初始化扩展，传入程序实例
 
@@ -65,10 +69,65 @@ def forge():
 
 
 ###################################flask网页生成(视图函数)######################################
-@app.route('/')
+#主页
+@app.route('/',methods = ['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        #获取表单数据
+        title = request.form.get('title')  #传入表单对应输入字段的name值
+        year = request.form.get('year')
+        #验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invaild input.')    #显示错误提示
+            return redirect(url_for('index'))    #重定向回主页
+        #保存表单数据到数据库
+        movie = Movie(title=title, year=year)    #创建记录
+        db.session.add(movie)    #添加到数据库会话
+        db.session.commit()      #提交数据库会话
+        flash('Item created.')   #显示成功创建的提示
+        return redirect(url_for('index'))    #重定向回主页
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies = movies)
+        
     movies = Movie.query.all()  # 读取所有电影记录
     return render_template('index.html', movies=movies)    #导入模板文件，并传入参数
+
+#记录编辑页
+@app.route('/movie/edit/<int:movie_id>', methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':    #处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash("Invalid input.")
+            return redirect(url_for('edit', movie_id=movie_id))
+        
+        
+        movie.title = title  #更新标题
+        movie.year = year    #更新年份
+        db.session.commit()  #提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))    #重定向回主页
+
+    return render_template('edit.html', movie=movie)    #传入被编辑的电影记录
+
+#删除电影条目
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])   #限定只接受POST请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)   #获取电影记录
+    db.session.delete(movie)     #删除对应的记录
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index')) #重定向到主页
+
+
+
+
+
+
 
 #######################################模板上下文处理函数#################################
 @app.context_processor
